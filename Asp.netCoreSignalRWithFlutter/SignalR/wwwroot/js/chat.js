3d429d277a29bb6157659e87dc5bf1212d4cf37e
MuaathMohammed <49982699+MuaathMohammed@users.noreply.github.com>
2021-08-11 04:39:08 +0300

    Add files via upload

---
+++ Asp.netCoreSignalRWithFlutter/SignalR/wwwroot/js/chat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = `${user} : ${message+"}`;
+    var lst = document.createElement("lst");
+    document.getElementById("messagesList").appendChild(lst);
+
+    lst.textContent = msg;
+    $('#messagesList').prepend(lst);
+
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+  //  event.preventDefault();
+});
